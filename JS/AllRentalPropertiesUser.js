df771c927a99428f03434b829c558791174913dc
Dhwani Naik <dnaik1@emich.edu>
2020-11-27 15:15:55 -0500

    Image Slider Done

--- JS/AllRentalPropertiesUser.js
+++ JS/AllRentalPropertiesUser.js
@@ -1,16 +1,13 @@
-var Images = [];
-function getPropertiesUser()
-{
+function getPropertiesUser() {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() 
-    {
-        if (this.readyState == 4 && this.status == 200) 
-        {
+    xmlhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+
             var properties = JSON.parse(this.responseText);
             properties.forEach(createProperty);
             
-            function createProperty(property, index)
-            {
+            function createProperty(property) {
                var property_key = property['ID'];
                var property_name = property['NAME'];
                var property_address1 = property['ADDRESS1'];
@@ -33,10 +30,11 @@ function getPropertiesUser()
                img_td_tag = document.createElement("td");
                img_tag = document.createElement("img");
                img_tag.src = "..\\PropertyImages\\"+img;
-               img_tag.width = "100";
-               img_tag.height = "100";
+               img_tag.width = "300";
+               img_tag.height = "150";
                img_tag.align = "center";
                img_tag.onclick = function (){
+                    var Images = [];
 					var xmlhttp = new XMLHttpRequest();
 					xmlhttp.open("POST", '../php/GetPropertyImages.php', true);
 					xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -47,31 +45,58 @@ function getPropertiesUser()
 							var propertiesImg = JSON.parse(this.responseText);
 							propertiesImg.forEach(createPropertyImages);
 							
-							function createPropertyImages(img)
+							function createPropertyImages(image)
 							{
-								Images.push(img["MEDIASRC"]);
-								imgSlider_tag = document.createElement("img");
-								imgSlider_tag.src = "..\\PropertyImages\\"+img["MEDIASRC"];
-								imgSlider_tag.width = "500";
-								//imgSlider_tag.height = "100%";
-								document.getElementById("imgSelected").appendChild(imgSlider_tag);
-								
-								img_div_tag = document.createElement("div");
-								img_div_tag.class = "column";
-								img_dot_tag = document.createElement("img");
-								img_dot_tag.src = "..\\PropertyImages\\"+img["MEDIASRC"];
-								img_dot_tag.className = "demo cursor";
-								img_div_tag.appendChild(img_dot_tag);
-								document.getElementById("imgContent").appendChild(img_div_tag);
-								openModal();
-								currentSlide(1);
-							}
-								
+                                Images.push(image["MEDIASRC"]);
+							}		
 						}   
 					}
 					xmlhttp.send('PROPERTY_KEY=' + property_key);
-					
-				}
+
+                    var imagePopup = document.getElementById("imagePopup");
+                    imagePopup.style.display = "block";
+
+                    var i = 0;
+
+                    image_slider_tag = document.getElementById("slider");
+	                image_slider_tag.src = "..\\PropertyImages\\"+Images[i];
+	                image_slider_tag.width = "400";
+	                image_slider_tag.height = "200";
+	                link_tag = document.getElementById("link");
+	                link_tag.href = "..\\PropertyImages\\"+Images[i];
+
+                    document.getElementById("next").onclick=function(){
+                        if(i < (Images.length - 1)){
+		                    i = i+1;
+	                    }else{
+		                    i = 0;
+	                    }
+	                    image_slider_tag.src = "..\\PropertyImages\\"+Images[i];
+	                    link_tag.href = "..\\PropertyImages\\"+Images[i];
+                    };
+
+                    document.getElementById("previous").onclick=function(){
+                        if(i > 0){
+		                    i = i-1;
+	                    }else{
+		                    i = (Images.length - 1);
+	                    }
+	                    image_slider_tag.src = "..\\PropertyImages\\"+Images[i];
+	                    link_tag.href = "..\\PropertyImages\\"+Images[i];
+                    };
+
+                    var span = document.getElementsByClassName("close")[0];
+                        span.onclick = function() {
+                            imagePopup.style.display = "none";
+                        };
+ 
+                     window.onclick = function(event) {
+                         if (event.target == imagePopup) {
+                             imagePopup.style.display = "none";
+                         } 
+                     };
+
+				};
                img_td_tag.appendChild(img_tag); 
                tr_tag.appendChild(img_td_tag);
 
@@ -135,41 +160,4 @@ function getPropertiesUser()
     };
     xmlhttp.open("GET", "../php/GetAvailableProperties.php", true);
     xmlhttp.send();
-}
-
-function openModal() {
-  document.getElementById("imgSlider").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("imgSlider").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
